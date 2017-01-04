96cb6254956b721b004c0d4114fbbaec8a5931d0
Waseem Ahmed <alphawaseem@gmail.com>
2017-01-04 19:34:38 +0530

    install angular to our project

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('index');
+    res.render('index.html');
 });
 
 module.exports = router;
