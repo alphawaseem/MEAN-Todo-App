45361c610be03bac3f5d98297d458ae2b621bb9f
Waseem Ahmed <alphawaseem@gmail.com>
2017-01-04 19:18:02 +0530

    update : use client folder in server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ let app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'client')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
